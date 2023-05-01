fb0960ddea8fa20251f30947a6463b29f7e6302d
Muhammad Saqib Nawaz <79707059+saqibsaqib123@users.noreply.github.com>
2023-05-01 09:35:33 +0500

    Saqib

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,31 @@ import Navbar from './components/Navbar';
 import ImgSlider from './components/ImgSlider';
 import Footer from './components/Footer';
 import Login from './components/Login';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  AuthErrorCodes,
+  getAuth,
+  signInWithEmailAndPassword
+} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3mBCz5MdvgQfESmeX1yzLqunSQ3HolDc",
+  authDomain: "clone-261bf.firebaseapp.com",
+  projectId: "clone-261bf",
+  storageBucket: "clone-261bf.appspot.com",
+  messagingSenderId: "205617641480",
+  appId: "1:205617641480:web:0ca15ff9e6f1cfa84279bd",
+  measurementId: "G-112YCVJJN9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
 
 const sliderImages = [
   'img/sliderimages/1.jpg',
@@ -19,13 +44,39 @@ const sliderImages = [
   'img/sliderimages/5.jpg',
 ]
 
+const showLoginError = (error) => {
+  document.getElementById('loginpassword').style.border = '1.5px solid red'
+  if (error.code === AuthErrorCodes.INVALID_PASSWORD) {
+    document.getElementById('showError').innerHTML = 'Wrong Password. Try again'
+  } else {
+    document.getElementById('showError').innerHTML = `Error: ${error.message}`
+  }
+  setTimeout(() => {
+    document.getElementById('showError').innerHTML = '';
+    document.getElementById('loginpassword').style.border = '1.5px solid grey'
+  }, 6000);
+}
+
+const loginEmailPassword = async (event) => {
+  event.preventDefault();
+  const signinEmail = document.getElementById('loginemail').value
+  const signinPassword = document.getElementById('loginpassword').value
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, signinEmail, signinPassword)
+    console.log(userCredential.user)
+  } catch (error) {
+    console.log(error);
+    showLoginError(error);
+  }
+}
+
 function App() {
   return (
     <Router>
 
       <Routes >
         <Route exact path="/" element={<>
-      <Navbar />
+          <Navbar />
           <ImgSlider images={sliderImages} />
           <div className='body'>
             <div className="row">
@@ -54,11 +105,14 @@ function App() {
               <Card1link title={'Books and media'} />
             </div>
           </div>
-      <Footer />
+          <Footer />
         </>} />
         <Route path="/signin" element={<>
-          <Login/>
-        </>}/>
+          <Login loginEmailPassword={loginEmailPassword} />
+        </>} />
+        <Route path="/signup" element={<>
+          <Login loginEmailPassword={loginEmailPassword} />
+        </>} />
       </Routes >
     </Router >
   );
--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -2,21 +2,22 @@ import React from 'react'
 import './Login.css'
 import { Link } from "react-router-dom";
 
-export default function Login() {
+export default function Login(props) {
     return (
-        <form>
+        <form onSubmit={props.loginEmailPassword} id='login'>
             <Link className='signin-logo' to={'/'}><img src="img/amazon-logo-black.png" alt="" /></Link>
             <div className="signin">
                 <h1>Sign in</h1>
                 <b><label htmlFor="email">Mobile phone number or email</label></b>
-                <input required type="email" />
+                <input id='loginemail' required autoComplete='username' type="email" />
                 <br />
                 <b><label htmlFor="password">Password</label></b>
-                <input required type="password" />
+                <input id='loginpassword' required autoComplete='current-password' type="password" />
+                <span style={{color: 'red'}} id="showError"></span>
                 <button type='submit'>Continue</button>
                 <p>This is an Amazon Clone made for Practice and <br />Fun purposes only!</p>
             </div>
-            <Link className='create-button' to="/">Create your Amazon account</Link>
+            <Link className='create-button' to="/signup">Create your Amazon account</Link>
             <div className="footer">
                 <span>&copy; This is an Amazon Clone made for Practice and Fun purposes only!</span>
             </div>
