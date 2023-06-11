5d0d709cf5d65514103b4d9e79491113a9483bf1
Muhammad Saqib Nawaz <79707059+saqibsaqib123@users.noreply.github.com>
2023-06-11 23:54:10 +0500

    Saqib

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import './Navbar.css'
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
